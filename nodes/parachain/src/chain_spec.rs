e892b3fce89271ec1ad56e782a63e28d3db3e063
Irman Nur Muhammad Alamsyah <irmannmal@gmail.com>
2021-07-11 18:45:28 +0700

    add new chainspec dev and staging parachain

--- nodes/parachain/src/chain_spec.rs
+++ nodes/parachain/src/chain_spec.rs
@@ -1,6 +1,6 @@
 use cumulus_primitives_core::ParaId;
 use myriad_parachain_runtime::{
-	AccountId, AuraId, SessionKeys, Signature, Balance, currency::MYRIA, WASM_BINARY,
+	AccountId, AuraId, Signature, Balance, currency::MYRIA, WASM_BINARY,
 	GenesisConfig, SystemConfig, BalancesConfig, SudoConfig, AuraConfig, ParachainInfoConfig
 };
 use sc_chain_spec::{ChainSpecExtension, ChainSpecGroup};
@@ -13,12 +13,6 @@ use sp_core::crypto::UncheckedInto;
 
 /// Specialized `ChainSpec` for the normal parachain runtime.
 pub type ChainSpec = sc_service::GenericChainSpec<GenesisConfig, Extensions>;
-
-fn session_keys(
-	aura: AuraId
-) -> SessionKeys {
-	SessionKeys { aura }
-}
 
 /// Generate a crypto pair from seed.
 pub fn get_from_seed<TPublic: Public>(seed: &str) -> <TPublic::Pair as Pair>::Public {
@@ -34,13 +28,6 @@ pub fn get_account_id_from_seed<TPublic: Public>(seed: &str) -> AccountId where
 	AccountPublic: From<<TPublic::Pair as Pair>::Public>
 {
 	AccountPublic::from(get_from_seed::<TPublic>(seed)).into_account()
-}
-
-/// Helper function to generate stash, controller and session key from seed
-pub fn authority_keys_from_seed(seed: &str) -> (AuraId) {
-	(
-		get_from_seed::<AuraId>(seed)
-	)
 }
 
 /// Helper function to generate an properties
@@ -70,31 +57,33 @@ impl Extensions {
 	}
 }
 
-pub fn local_development_tesnet_config(id: ParaId) -> Result<ChainSpec, String> {
+pub fn staging_tesnet_config(id: ParaId) -> Result<ChainSpec, String> {
 	let wasm_binary = WASM_BINARY.ok_or_else(|| "WASM not available".to_string())?;
 	let properties = get_properties("MYRIA", 18, 42);
 
 	Ok(ChainSpec::from_genesis(
 		// Name
-		"Myriad Local Development Testnet",
+		"Myriad Staging Testnet",
 		// ID
-		"myriad_local_development_testnet",
-		ChainType::Development,
+		"myriad_staging_testnet",
+		ChainType::Live,
 		move || testnet_genesis(
 			// WASM Binary
 			wasm_binary,
 			// Sudo account
-			get_account_id_from_seed::<sr25519::Public>("Alice"),
+			// 5HVgMkXJGoDGQdnTyah4shbhuaiNCmAUdqCyTdYAnr9T9Y1Q
+			hex!["f03941f93b990c271015d3b485f137e117aab80af0a03b557966927caaa7d44f"].into(),
 			// Initial PoA authorities
 			vec![
-				authority_keys_from_seed("Alice"),
+				//5GhTbhujpv3nZQx6idibYSwYeNCN7ddpqqjPjwZn43xdvYMT
+				hex!["ccf90463ce9ae4cf881c549b09ddeac1960316930e390ca47eeba95741386e5b"].unchecked_into(),
+				//5H9RP9sy2g9Jaj1GG2zGaytLdxoBHQnqMaKmqvtFPJpYiRV3
+				hex!["e0c5efc09df70c2e236e32ebba4c89a5ae538dacf25412e2a23e6a175291453a"].unchecked_into(),
 			],
 			// Pre-funded accounts
 			vec![
-				get_account_id_from_seed::<sr25519::Public>("Alice"),
-				get_account_id_from_seed::<sr25519::Public>("Bob"),
-				get_account_id_from_seed::<sr25519::Public>("Alice//stash"),
-				get_account_id_from_seed::<sr25519::Public>("Bob//stash"),
+				// 5HVgMkXJGoDGQdnTyah4shbhuaiNCmAUdqCyTdYAnr9T9Y1Q
+				hex!["f03941f93b990c271015d3b485f137e117aab80af0a03b557966927caaa7d44f"].into(),
 			],
 			id,
 		),
@@ -103,7 +92,51 @@ pub fn local_development_tesnet_config(id: ParaId) -> Result<ChainSpec, String> 
 		// Telemetry
 		None,
 		// Protocol ID
-		Some("myriad-local-development-tesnet".into()),
+		Some("myriad-staging-tesnet".into()),
+		// Properties
+		Some(properties),
+		// Extensions
+		Extensions {
+			relay_chain: "rococo-staging".into(), // You MUST set this to the correct network!
+			para_id: id.into(),
+		},
+	))
+}
+
+pub fn development_tesnet_config(id: ParaId) -> Result<ChainSpec, String> {
+	let wasm_binary = WASM_BINARY.ok_or_else(|| "WASM not available".to_string())?;
+	let properties = get_properties("MYRIA", 18, 42);
+
+	Ok(ChainSpec::from_genesis(
+		// Name
+		"Myriad Development Testnet",
+		// ID
+		"myriad_development_testnet",
+		ChainType::Live,
+		move || testnet_genesis(
+			// WASM Binary
+			wasm_binary,
+			// Sudo account
+			// 5EZYLWe1j3MjuH1vJf6Mc5CxaeGfVeoAQn3DwYuLvABDYU1U
+			hex!["6e768960d4a61b5583eb76ac22ba91dce97ef55fa8ca4b764c774cdb9af93b36"].into(),
+			// Initial PoA authorities
+			vec![
+				//5Gx1QL5a18H63ofyYdZhjpiTKA9XCgpfoTCztT2dpKsHQE9j
+				hex!["d811839e01e3cc6eeb64e6f312a1eaf2988ae2c5fea9dd0b8ac018c146ca7073"].unchecked_into(),
+			],
+			// Pre-funded accounts
+			vec![
+				// 5EZYLWe1j3MjuH1vJf6Mc5CxaeGfVeoAQn3DwYuLvABDYU1U
+				hex!["6e768960d4a61b5583eb76ac22ba91dce97ef55fa8ca4b764c774cdb9af93b36"].into(),
+			],
+			id,
+		),
+		// Bootnodes
+		vec![],
+		// Telemetry
+		None,
+		// Protocol ID
+		Some("myriad-development-tesnet".into()),
 		// Properties
 		Some(properties),
 		// Extensions
@@ -131,8 +164,8 @@ pub fn local_testnet_config(id: ParaId) -> Result<ChainSpec, String> {
 			get_account_id_from_seed::<sr25519::Public>("Alice"),
 			// Initial PoA authorities
 			vec![
-				authority_keys_from_seed("Alice"),
-				authority_keys_from_seed("Bob"),
+				get_from_seed::<AuraId>("Alice"),
+				get_from_seed::<AuraId>("Bob"),
 			],
 			// Pre-funded accounts
 			vec![
@@ -142,12 +175,6 @@ pub fn local_testnet_config(id: ParaId) -> Result<ChainSpec, String> {
 				get_account_id_from_seed::<sr25519::Public>("Dave"),
 				get_account_id_from_seed::<sr25519::Public>("Eve"),
 				get_account_id_from_seed::<sr25519::Public>("Ferdie"),
-				get_account_id_from_seed::<sr25519::Public>("Alice//stash"),
-				get_account_id_from_seed::<sr25519::Public>("Bob//stash"),
-				get_account_id_from_seed::<sr25519::Public>("Charlie//stash"),
-				get_account_id_from_seed::<sr25519::Public>("Dave//stash"),
-				get_account_id_from_seed::<sr25519::Public>("Eve//stash"),
-				get_account_id_from_seed::<sr25519::Public>("Ferdie//stash"),
 			],
 			id,
 		),
@@ -167,10 +194,56 @@ pub fn local_testnet_config(id: ParaId) -> Result<ChainSpec, String> {
 	))
 }
 
+pub fn local_development_tesnet_config(id: ParaId) -> Result<ChainSpec, String> {
+	let wasm_binary = WASM_BINARY.ok_or_else(|| "WASM not available".to_string())?;
+	let properties = get_properties("MYRIA", 18, 42);
+
+	Ok(ChainSpec::from_genesis(
+		// Name
+		"Myriad Local Development Testnet",
+		// ID
+		"myriad_local_development_testnet",
+		ChainType::Development,
+		move || testnet_genesis(
+			// WASM Binary
+			wasm_binary,
+			// Sudo account
+			get_account_id_from_seed::<sr25519::Public>("Alice"),
+			// Initial PoA authorities
+			vec![
+				get_from_seed::<AuraId>("Alice"),
+			],
+			// Pre-funded accounts
+			vec![
+				get_account_id_from_seed::<sr25519::Public>("Alice"),
+				get_account_id_from_seed::<sr25519::Public>("Bob"),
+				get_account_id_from_seed::<sr25519::Public>("Charlie"),
+				get_account_id_from_seed::<sr25519::Public>("Dave"),
+				get_account_id_from_seed::<sr25519::Public>("Eve"),
+				get_account_id_from_seed::<sr25519::Public>("Ferdie"),
+			],
+			id,
+		),
+		// Bootnodes
+		vec![],
+		// Telemetry
+		None,
+		// Protocol ID
+		Some("myriad-local-development-tesnet".into()),
+		// Properties
+		Some(properties),
+		// Extensions
+		Extensions {
+			relay_chain: "rococo-dev".into(), // You MUST set this to the correct network!
+			para_id: id.into(),
+		},
+	))
+}
+
 fn testnet_genesis(
 	wasm_binary: &[u8],
 	root_key: AccountId,
-	initial_authorities: Vec<(AuraId)>,
+	initial_authorities: Vec<AuraId>,
 	endowed_accounts: Vec<AccountId>,
 	id: ParaId,
 ) -> GenesisConfig {
