9190948551befdb371ad213939e6cb67a2a26e40
Irman Nur Muhammad Alamsyah <irmannmal@gmail.com>
2021-07-11 20:20:29 +0700

    build all

---
+++ .maintain/build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+
+pushd .
+
+# The following line ensure we run from the project root
+PROJECT_ROOT=`git rev-parse --show-toplevel`
+cd $PROJECT_ROOT
+
+echo "Building binary"
+time cargo build --release
