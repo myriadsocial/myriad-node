db8daa5c7a94909b8e63d351b53b545abf3f9cc1
Irman Nur Muhammad Alamsyah <irmannmal@gmail.com>
2021-07-11 20:19:29 +0700

    start node script

---
+++ .maintain/start-appchain.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -e
+
+pushd .
+
+# The following line ensure we run from the project root
+PROJECT_ROOT=`git rev-parse --show-toplevel`
+cd $PROJECT_ROOT
+
+echo "Starting node..."
+./target/debug/myriad-appchain \
+-d .local/appchain \
+--dev \
+--enable-offchain-indexing true
