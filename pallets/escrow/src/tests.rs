dbcef566ea5e6a63c7730a1a25d4c8335246cc0b
abdulhakim2902 <hakim@blocksphere.id>
2021-09-25 11:04:47 +0700

    added testing for pallet escrow

---
+++ pallets/escrow/src/tests.rs
@@ -0,0 +1,156 @@
+use crate::{
+	self as pallet_escrow,
+	mock::{Event, *},
+	Error,
+};
+use frame_support::{assert_noop, assert_ok};
+
+#[test]
+fn donate_works() {
+	ExternalityBuilder::build().execute_with(|| {
+		assert_ok!(Currencies::add_currency(
+			Origin::root(),
+			String::from("ACA").into_bytes(),
+			String::from("acala").into_bytes(),
+			12,
+			String::from("wss://rpc.myriad.systems").into_bytes(),
+			true
+		));
+		assert_ok!(Currencies::update_balance(
+			Origin::root(),
+			1,
+			String::from("ACA").into_bytes(),
+			21000000
+		));
+		assert_ok!(Platform::add_platform(Origin::root(), String::from("twitter").into_bytes()));
+		assert_ok!(Escrow::donate(
+			Origin::signed(1),
+			String::from("ACA").into_bytes(),
+			pallet_escrow::Post {
+				post_id: String::from("60efac8c565ab8004ed28bb3").into_bytes(),
+				people_id: String::from("60efac8c565ab8004ed28ba6").into_bytes(),
+				platform: String::from("twitter").into_bytes()
+			},
+			100
+		));
+		assert_ok!(Escrow::donate(
+			Origin::signed(1),
+			String::from("ACA").into_bytes(),
+			pallet_escrow::Post {
+				post_id: String::from("60efac8c565ab8004ed28bb5").into_bytes(),
+				people_id: String::from("60efac8c565ab8004ed28ba6").into_bytes(),
+				platform: String::from("twitter").into_bytes()
+			},
+			100
+		));
+
+		assert_eq!(
+			Currencies::accounts(1, String::from("ACA").into_bytes()),
+			Some(pallet_currency::CurrencyBalance { free: 20999800 })
+		);
+		assert_eq!(
+			Currencies::accounts(Escrow::account_id(), String::from("ACA").into_bytes()),
+			Some(pallet_currency::CurrencyBalance { free: 200 })
+		);
+		assert_eq!(
+			Escrow::people_balance((
+				String::from("ACA").into_bytes(),
+				String::from("60efac8c565ab8004ed28ba6").into_bytes(),
+				String::from("twitter").into_bytes()
+			)),
+			200
+		);
+		assert_eq!(
+			Escrow::post_balance((
+				String::from("ACA").into_bytes(),
+				String::from("60efac8c565ab8004ed28bb3").into_bytes(),
+				String::from("60efac8c565ab8004ed28ba6").into_bytes(),
+				String::from("twitter").into_bytes()
+			)),
+			100
+		);
+		assert_eq!(
+			Escrow::post_balance((
+				String::from("ACA").into_bytes(),
+				String::from("60efac8c565ab8004ed28bb5").into_bytes(),
+				String::from("60efac8c565ab8004ed28ba6").into_bytes(),
+				String::from("twitter").into_bytes()
+			)),
+			100
+		);
+	});
+}
+
+#[test]
+fn cant_donate_when_platform_not_exist() {
+	ExternalityBuilder::build().execute_with(|| {
+		assert_ok!(Currencies::add_currency(
+			Origin::root(),
+			String::from("ACA").into_bytes(),
+			String::from("acala").into_bytes(),
+			12,
+			String::from("wss://rpc.myriad.systems").into_bytes(),
+			true
+		));
+		assert_ok!(Currencies::update_balance(
+			Origin::root(),
+			1,
+			String::from("ACA").into_bytes(),
+			21000000
+		));
+
+		assert_noop!(
+			Escrow::donate(
+				Origin::signed(1),
+				String::from("ACA").into_bytes(),
+				pallet_escrow::Post {
+					post_id: String::from("60efac8c565ab8004ed28bb3").into_bytes(),
+					people_id: String::from("60efac8c565ab8004ed28ba6").into_bytes(),
+					platform: String::from("twitter").into_bytes()
+				},
+				100
+			),
+			Error::<Test>::PlatformNotExist
+		);
+	})
+}
+
+#[test]
+fn call_event_should_work() {
+	ExternalityBuilder::build().execute_with(|| {
+		System::set_block_number(1);
+
+		assert_ok!(Currencies::add_currency(
+			Origin::root(),
+			String::from("ACA").into_bytes(),
+			String::from("acala").into_bytes(),
+			12,
+			String::from("wss://rpc.myriad.systems").into_bytes(),
+			true
+		));
+		assert_ok!(Currencies::update_balance(
+			Origin::root(),
+			1,
+			String::from("ACA").into_bytes(),
+			21000000
+		));
+		assert_ok!(Platform::add_platform(Origin::root(), String::from("twitter").into_bytes()));
+		assert_ok!(Escrow::donate(
+			Origin::signed(1),
+			String::from("ACA").into_bytes(),
+			pallet_escrow::Post {
+				post_id: String::from("60efac8c565ab8004ed28bb3").into_bytes(),
+				people_id: String::from("60efac8c565ab8004ed28ba6").into_bytes(),
+				platform: String::from("twitter").into_bytes()
+			},
+			100
+		));
+		System::assert_last_event(Event::Escrow(crate::Event::DonationReceived(
+			String::from("ACA").into_bytes(),
+			1,
+			Escrow::account_id(),
+			100,
+			pallet_currency::CurrencyBalance { free: 100 },
+		)));
+	})
+}
