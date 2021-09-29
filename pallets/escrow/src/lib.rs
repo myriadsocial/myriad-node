61fcca977748487fd77663a8f6a3ff42cbe8e9db
abdulhakim2902 <hakim@blocksphere.id>
2021-09-29 17:03:40 +0700

    refactor pallet escrow

--- pallets/escrow/src/lib.rs
+++ pallets/escrow/src/lib.rs
@@ -8,6 +8,9 @@ mod mock;
 #[cfg(test)]
 mod tests;
 
+#[cfg(feature = "runtime-benchmarks")]
+mod benchmarking;
+
 #[frame_support::pallet]
 pub mod pallet {
 	use frame_support::{
@@ -38,17 +41,13 @@ pub mod pallet {
 	#[pallet::metadata(T::AccountId = "AccountId")]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
 	pub enum Event<T: Config> {
-		/// Currency donate success. \[currency_id, donor, pot, amount, charity_balance\]
+		/// Currency donate success. \[currency_id, pot, donor, amount, pot_balance\]
 		DonationReceived(CurrencyId, T::AccountId, T::AccountId, Amount, CurrencyBalance),
-		/// Fund allocate success. \[currency_id, to, amount, charity_balance\]
-		FundsAllocated(CurrencyId, T::AccountId, Amount, CurrencyBalance),
 	}
 
 	#[pallet::error]
 	pub enum Error<T> {
 		InsufficientAmount,
-		InsufficientFunds,
-		OverflowAmount,
 		CurrencyNotExist,
 		PlatformNotExist,
 	}
@@ -106,84 +105,11 @@ pub mod pallet {
 			ensure!(currency.is_some(), Error::<T>::CurrencyNotExist);
 			ensure!(amount > 0, Error::<T>::InsufficientAmount);
 
-			let sender_balance = pallet_currency::Pallet::<T>::accounts(&donor, &currency_id)
-				.unwrap_or(CurrencyBalance { free: 0 });
-			let receiver_balance =
-				pallet_currency::Pallet::<T>::accounts(Self::account_id(), &currency_id)
-					.unwrap_or(CurrencyBalance { free: 0 });
-
-			// Calculate new balances
-			let updated_from_balance =
-				sender_balance.free.checked_sub(amount).ok_or(Error::<T>::InsufficientFunds)?;
-			let updated_to_balance =
-				receiver_balance.free.checked_add(amount).ok_or(Error::<T>::OverflowAmount)?;
-
-			// Write new balances to storage
-			pallet_currency::Accounts::<T>::insert(
-				&donor,
-				&currency_id,
-				CurrencyBalance { free: updated_from_balance },
-			);
-
-			pallet_currency::Accounts::<T>::insert(
-				Self::account_id(),
-				&currency_id,
-				CurrencyBalance { free: updated_to_balance },
-			);
-
 			Self::do_update_balance(post, amount, currency_id.clone());
 			Self::deposit_event(Event::DonationReceived(
 				currency_id.clone(),
 				donor,
 				Self::account_id(),
-				amount,
-				Self::pot(&currency_id),
-			));
-			Ok(().into())
-		}
-
-		/// Allocate the Charity's funds
-		///
-		/// Take funds from the Charity's pot and send them somewhere. This call requires root origin,
-		/// which means it must come from a governance mechanism such as Substrate's Democracy pallet.
-		#[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
-		pub fn allocate(
-			origin: OriginFor<T>,
-			dest: T::AccountId,
-			currency_id: CurrencyId,
-			amount: u128,
-		) -> DispatchResultWithPostInfo {
-			ensure_root(origin)?;
-			let currency = pallet_currency::Pallet::<T>::currency(currency_id.clone());
-
-			ensure!(currency.is_some(), Error::<T>::CurrencyNotExist);
-
-			// Make the transfer requested
-			let pot_balance = Self::pot(&currency_id);
-			let dest_balance = pallet_currency::Pallet::<T>::accounts(&dest, &currency_id)
-				.unwrap_or(CurrencyBalance { free: 0 });
-
-			ensure!(amount <= pot_balance.free, Error::<T>::InsufficientAmount);
-
-			let updated_pot_balance = pot_balance.free.checked_sub(amount);
-			let updated_dest_balance =
-				dest_balance.free.checked_add(amount).ok_or(Error::<T>::OverflowAmount)?;
-
-			pallet_currency::Accounts::<T>::insert(
-				&Self::account_id(),
-				&currency_id,
-				CurrencyBalance { free: updated_pot_balance.unwrap_or(0) },
-			);
-
-			pallet_currency::Accounts::<T>::insert(
-				&dest,
-				&currency_id,
-				CurrencyBalance { free: updated_dest_balance },
-			);
-
-			Self::deposit_event(Event::FundsAllocated(
-				currency_id.clone(),
-				dest,
 				amount,
 				Self::pot(&currency_id),
 			));
@@ -209,19 +135,29 @@ pub mod pallet {
 			// Get balances
 			let people_balance = Self::people_balance((&currency_id, &people_id, &platform));
 			let post_balance = Self::post_balance((&currency_id, &post_id, &people_id, &platform));
+			let pot_balance =
+				pallet_currency::Pallet::<T>::accounts(Self::account_id(), &currency_id)
+					.unwrap_or(CurrencyBalance { free: 0 });
 
 			// Calculate people and post balance
-			let updated_people_balance = people_balance.checked_add(amount);
-			let updated_post_balance = post_balance.checked_add(amount);
+			let updated_people_balance = people_balance.checked_add(amount).unwrap_or(0);
+			let updated_post_balance = post_balance.checked_add(amount).unwrap_or(0);
+			let updated_pot_balance = pot_balance.free.checked_add(amount).unwrap_or(0);
 
 			// Store in storage
 			PeopleBalance::<T>::insert(
 				(&currency_id, &people_id, &platform),
-				updated_people_balance.unwrap_or(0),
+				updated_people_balance,
 			);
 			PostBalance::<T>::insert(
 				(&currency_id, &post_id, &people_id, &platform),
-				updated_post_balance.unwrap_or(0),
+				updated_post_balance,
+			);
+
+			pallet_currency::Accounts::<T>::insert(
+				Self::account_id(),
+				&currency_id,
+				CurrencyBalance { free: updated_pot_balance },
 			);
 		}
 	}
