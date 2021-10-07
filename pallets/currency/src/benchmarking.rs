5404e40fb7ccc50e08b7e59c4f068aa650e3cfef
abdulhakim2902 <hakim@blocksphere.id>
2021-10-07 19:36:01 +0700

    fixed pallet benchmarking

--- pallets/currency/src/benchmarking.rs
+++ pallets/currency/src/benchmarking.rs
@@ -22,6 +22,10 @@ benchmarks! {
 	update_balance {
 		let s in 1 .. 100;
 		let caller: T::AccountId = whitelisted_caller();
+		let caller_origin = <T as frame_system::Config>::Origin::from(RawOrigin::Signed(caller.clone()));
+
+		let _add_currency = Currency::<T>::add_currency(caller_origin.clone(), vec![b'X';256], 18, vec![b'X';256], true);
+
 		let recipient: T::AccountId = account("recepient", 0, SEED);
 	}: update_balance(
 		RawOrigin::Signed(caller),
@@ -33,6 +37,11 @@ benchmarks! {
 	transfer {
 		let s in 1 .. 100;
 		let caller: T::AccountId = whitelisted_caller();
+		let caller_origin = <T as frame_system::Config>::Origin::from(RawOrigin::Signed(caller.clone()));
+
+		let _add_currency = Currency::<T>::add_currency(caller_origin.clone(), vec![b'X';256], 18, vec![b'X';256], true);
+		let _insert_balance = Currency::<T>::update_balance(caller_origin.clone(), caller.clone(), vec![b'X';256], 10000u128.into());
+
 		let recipient: T::AccountId = account("recepient", 0, SEED);
 	}: transfer(
 		RawOrigin::Signed(caller),
--- pallets/escrow/src/benchmarking.rs
+++ pallets/escrow/src/benchmarking.rs
@@ -8,14 +8,16 @@ use sp_std::vec;
 
 benchmarks! {
 	send_tip {
+		let s in 1 .. 100;
+		let caller: T::AccountId = whitelisted_caller();
 		let post = Post {
 			post_id: vec![b'X';256],
 			people_id: vec![b'X';256],
 			platform: vec![b'X';256],
 		};
-
-		let s in 1 .. 100;
-		let caller: T::AccountId = whitelisted_caller();
+		let caller_origin = <T as frame_system::Config>::Origin::from(RawOrigin::Signed(caller.clone()));
+		let _add_platform = pallet_platform::Pallet::<T>::add_platform(caller_origin.clone(), vec![b'X';256]);
+		let _add_currency = pallet_currency::Pallet::<T>::add_currency(caller_origin.clone(), vec![b'X';256], 18, vec![b'X';256], true);
 	}: send_tip(RawOrigin::Signed(caller), post, vec![b'X';256], s.into())
 }
 
