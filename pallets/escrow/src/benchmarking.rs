c921d6401bd38fa35ba27dbc655e0a38cac65c2f
abdulhakim2902 <hakim@blocksphere.id>
2021-10-07 10:00:21 +0700

    fixed benchmarking

--- pallets/escrow/src/benchmarking.rs
+++ pallets/escrow/src/benchmarking.rs
@@ -4,18 +4,19 @@ use super::*;
 use crate::Pallet as Escrow;
 use frame_benchmarking::{benchmarks, impl_benchmark_test_suite, whitelisted_caller};
 use frame_system::RawOrigin;
+use sp_std::vec;
 
 benchmarks! {
 	send_tip {
 		let post = Post {
-			post_id: String::from("60efac8c565ab8004ed28bb3").into_bytes(),
-			people_id: String::from("60efac8c565ab8004ed28ba6").into_bytes(),
-			platform: String::from("twitter").into_bytes()
+			post_id: vec![b'X';256],
+			people_id: vec![b'X';256],
+			platform: vec![b'X';256],
 		};
 
 		let s in 1 .. 100;
 		let caller: T::AccountId = whitelisted_caller();
-	}: send_tip(RawOrigin::Signed(caller), post, String::from("MYRIA").into_bytes(), s.into())
+	}: send_tip(RawOrigin::Signed(caller), post, vec![b'X';256], s.into())
 }
 
 impl_benchmark_test_suite! {Escrow, crate::mock::ExternalityBuilder::build(), crate::mock::Test}
