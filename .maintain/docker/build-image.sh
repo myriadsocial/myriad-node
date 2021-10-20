c994eff1e3a9e9f7ae0eafd18362597a8c2f0284
Irman Nur Muhammad Alamsyah <irmannmal@gmail.com>
2021-10-20 19:11:25 +0800

    CI/CD: upgraded to ubuntu 20.04

--- .maintain/docker/build-image.sh
+++ .maintain/docker/build-image.sh
@@ -10,17 +10,9 @@ cd $PROJECT_ROOT
 
 IMAGE_NAME=myriadsocial/myriad-node
 
-# Copy binary
-echo "Copiying binary"
-time cp ./target/release/myriad .
-
 # Build the image
 echo "Building ${IMAGE_NAME}:latest docker image, hang on!"
 time docker build -f ./.maintain/docker/Dockerfile -t ${IMAGE_NAME}:latest .
-
-# Remove binary
-echo "Removing binary"
-time rm ./myriad
 
 # Show the list of available images for this repo
 echo "Image is ready"
