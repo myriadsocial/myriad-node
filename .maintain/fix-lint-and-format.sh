73a1b3e80e94918198576d3a74f83a0fae91325a
Irman Nur Muhammad Alamsyah <irmannmal@gmail.com>
2021-11-09 21:21:53 +0800

    fixed lint and format script

--- .maintain/fix-lint-and-format.sh
+++ .maintain/fix-lint-and-format.sh
@@ -9,7 +9,7 @@ PROJECT_ROOT=`git rev-parse --show-toplevel`
 cd $PROJECT_ROOT
 
 echo "Fix Lint"
-cargo +nightly clippy --all
+cargo +nightly clippy --all --fix
 echo "Fix Format"
 cargo +nightly fmt --all
 
