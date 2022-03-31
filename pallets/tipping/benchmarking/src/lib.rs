e7b11f39c022f8d4ebe3d09b88ea43012911b948
abdulhakim2902 <hakim@blocksphere.id>
2022-03-31 21:06:15 +0700

    MYR-1345: added benchmarks for tipping pallet

---
+++ pallets/tipping/benchmarking/src/lib.rs
@@ -0,0 +1,85 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+mod mock;
+
+const SEED: u32 = 0;
+
+use frame_benchmarking::{account, benchmarks, whitelisted_caller};
+use frame_support::{sp_runtime::SaturatedConversion, traits::Currency};
+use frame_system::RawOrigin;
+use pallet_server::{AdminKey, Config as ServerConfig, Pallet as Server};
+#[allow(unused)]
+use pallet_tipping::{
+	Call, Config as TippingConfig, Pallet as Tipping, TipsBalance, TipsBalanceInfo,
+};
+
+pub struct Pallet<T: Config>(Tipping<T>);
+
+pub trait Config: TippingConfig + ServerConfig {}
+
+benchmarks! {
+	send_tip {
+		let s in 1 .. 100;
+		let caller: T::AccountId = whitelisted_caller();
+		let _ = <T as TippingConfig>::Currency::deposit_creating(&caller, 1000000000000000000000u128.saturated_into());
+
+		let admin: T::AccountId = AdminKey::<T>::get();
+		let server_account: T::AccountId = account("server_account", 0, SEED);
+		let server_name = "myriad".as_bytes().to_vec();
+		let admin_origin = <T as frame_system::Config>::Origin::from(RawOrigin::Signed(admin));
+		let server_id = Server::<T>::generate_server_id(&server_account, &server_name);
+		let _server = Server::<T>::register(admin_origin, server_account, server_name);
+
+		let reference_id = "people_id".as_bytes().to_vec();
+		let reference_type = "people".as_bytes().to_vec();
+		let ft_identifier = "native".as_bytes().to_vec();
+		let tips_balance_info = TipsBalanceInfo::new(&server_id, &reference_type, &reference_id, &ft_identifier);
+	}: _(RawOrigin::Signed(caller), tips_balance_info, s.into())
+
+	claim_reference {
+		let caller: T::AccountId = whitelisted_caller();
+
+		let admin: T::AccountId = AdminKey::<T>::get();
+		let admin_origin = <T as frame_system::Config>::Origin::from(RawOrigin::Signed(admin));
+		let server_name = "myriad".as_bytes().to_vec();
+		let server_id = Server::<T>::generate_server_id(&caller, &server_name);
+		let _server = Server::<T>::register(admin_origin, caller.clone(), server_name);
+
+		let reference_id = "people_id".as_bytes().to_vec();
+		let reference_type = "people".as_bytes().to_vec();
+		let ft_identifier = "native".as_bytes().to_vec();
+		let tips_balance_info = TipsBalanceInfo::new(&server_id, &reference_type, &reference_id, &ft_identifier);
+	}: _(RawOrigin::Signed(caller), tips_balance_info, "user".as_bytes().to_vec(), "user_id".as_bytes().to_vec(), None)
+
+	claim_tip {
+		let caller: T::AccountId = whitelisted_caller();
+		let tipping_account_id = Tipping::<T>::tipping_account_id();
+
+		let _ = <T as TippingConfig>::Currency::deposit_creating(&caller, 1000000000000000000000u128.saturated_into());
+		let _ = <T as TippingConfig>::Currency::deposit_creating(&tipping_account_id, 1000000000000000000000u128.saturated_into());
+
+		// Register Server
+		// Server admin => server_account
+		let admin: T::AccountId = AdminKey::<T>::get();
+		let server_account: T::AccountId = account("server_account", 0, SEED);
+		let admin_origin = <T as frame_system::Config>::Origin::from(RawOrigin::Signed(admin));
+		let server_origin = <T as frame_system::Config>::Origin::from(RawOrigin::Signed(server_account.clone()));
+		let server_id = Server::<T>::generate_server_id(&server_account, "myriad".as_bytes());
+
+		let _ = Server::<T>::register(admin_origin, server_account, "myriad".as_bytes().to_vec());
+
+		// Send Tipping
+		let account_1: T::AccountId = account("account", 0, SEED);
+		let account_1_origin = <T as frame_system::Config>::Origin::from(RawOrigin::Signed(account_1.clone()));
+		let tips_balance_info = TipsBalanceInfo::new(&server_id, "people".as_bytes(), "people_id".as_bytes(), "native".as_bytes());
+
+		let _ = <T as TippingConfig>::Currency::deposit_creating(&account_1, 1000000000000000000000u128.saturated_into());
+		let _ = Tipping::<T>::send_tip(account_1_origin, tips_balance_info.clone(), 10000000000000000000u128.saturated_into());
+
+		// Claim Reference
+		let tips_balance_info_user = TipsBalanceInfo::new(&server_id, "user".as_bytes(), "user_id".as_bytes(), "native".as_bytes());
+
+		let _ = Tipping::<T>::claim_reference(server_origin.clone(), tips_balance_info_user.clone(), "user".as_bytes().to_vec(), "user_id".as_bytes().to_vec(), Some(caller.clone()));
+		let _ = Tipping::<T>::claim_reference(server_origin, tips_balance_info, "user".as_bytes().to_vec(), "user_id".as_bytes().to_vec(), None);
+	}: _(RawOrigin::Signed(caller), tips_balance_info_user)
+}
---
+++ pallets/tipping/benchmarking/src/mock.rs
@@ -0,0 +1,91 @@
+#![cfg(test)]
+
+use frame_support::{construct_runtime, parameter_types, traits::Everything};
+use frame_system as system;
+use pallet_balances::AccountData;
+use sp_core::H256;
+use sp_runtime::{
+	testing::Header,
+	traits::{BlakeTwo256, IdentityLookup},
+};
+
+type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Test>;
+type Block = frame_system::mocking::MockBlock<Test>;
+
+construct_runtime!(
+	pub enum Test where
+		Block = Block,
+		NodeBlock = Block,
+		UncheckedExtrinsic = UncheckedExtrinsic,
+	{
+		System: frame_system::{Pallet, Call, Config, Storage, Event<T>},
+		Balances: pallet_balances::{Pallet, Call, Storage, Config<T>, Event<T>},
+		Tipping: pallet_tipping::{Pallet, Call, Storage, Event<T>},
+		Server: pallet_server::{Pallet, Call, Storage, Event<T>},
+	}
+);
+
+parameter_types! {
+	pub const BlockHashCount: u64 = 250;
+	pub const SS58Prefix: u8 = 42;
+	pub BlockWeights: frame_system::limits::BlockWeights =
+		frame_system::limits::BlockWeights::simple_max(1024);
+}
+
+impl system::Config for Test {
+	type BaseCallFilter = Everything;
+	type BlockWeights = ();
+	type BlockLength = ();
+	type Origin = Origin;
+	type Index = u64;
+	type Call = Call;
+	type BlockNumber = u64;
+	type Hash = H256;
+	type Hashing = BlakeTwo256;
+	type AccountId = u64;
+	type Lookup = IdentityLookup<Self::AccountId>;
+	type Header = Header;
+	type Event = Event;
+	type BlockHashCount = BlockHashCount;
+	type DbWeight = ();
+	type Version = ();
+	type PalletInfo = PalletInfo;
+	type AccountData = AccountData<Balance>;
+	type OnNewAccount = ();
+	type OnKilledAccount = ();
+	type SystemWeightInfo = ();
+	type SS58Prefix = SS58Prefix;
+	type OnSetCode = ();
+}
+
+type Balance = u128;
+
+parameter_types! {
+	pub static ExistentialDeposit: Balance = 0;
+}
+
+impl pallet_balances::Config for Test {
+	type MaxLocks = ();
+	type MaxReserves = ();
+	type ReserveIdentifier = [u8; 8];
+	/// The type for recording an account's balance.
+	type Balance = Balance;
+	/// The ubiquitous event type.
+	type Event = Event;
+	type DustRemoval = ();
+	type ExistentialDeposit = ExistentialDeposit;
+	type AccountStore = System;
+	type WeightInfo = ();
+}
+
+impl pallet_tipping::Config for Test {
+	type Event = Event;
+	type Currency = Balances;
+	type Server = Server;
+	type WeightInfo = ();
+}
+
+impl pallet_server::Config for Test {
+	type Event = Event;
+	type WeightInfo = ();
+}
