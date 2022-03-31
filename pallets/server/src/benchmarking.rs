1e7c2d28167de431657c9a16684031692c0d4e71
abdulhakim2902 <hakim@blocksphere.id>
2022-03-31 15:23:53 +0700

    MYR-1877: implement admin key to pallet call

--- pallets/server/src/benchmarking.rs
+++ pallets/server/src/benchmarking.rs
@@ -3,7 +3,7 @@
 use super::*;
 
 #[allow(unused)]
-use crate::{Pallet as Server, ServerInterface};
+use crate::{AdminKey, Pallet as Server, ServerInterface};
 use frame_benchmarking::{account, benchmarks, impl_benchmark_test_suite, whitelisted_caller};
 use frame_system::RawOrigin;
 use sp_std::vec;
@@ -13,31 +13,40 @@ const SEED: u32 = 0;
 benchmarks! {
 	register {
 		let s in 1 .. 100;
-		let caller: T::AccountId = whitelisted_caller();
-	}: register(RawOrigin::Signed(caller), vec![s as u8])
+		let caller: T::AccountId = AdminKey::<T>::get();
+		let account_id: T::AccountId = whitelisted_caller();
+	}: _(RawOrigin::Signed(caller), account_id, vec![s as u8])
 
 	update_name {
 		let s in 1 .. 100;
-		let caller: T::AccountId = whitelisted_caller();
+		let caller: T::AccountId = AdminKey::<T>::get();
+		let account_id: T::AccountId = whitelisted_caller();
 		let caller_origin = <T as frame_system::Config>::Origin::from(RawOrigin::Signed(caller.clone()));
-		let server_id = Server::<T>::generate_server_id(&caller, &"myriad".as_bytes().to_vec());
-		let _server = Server::<T>::register(caller_origin.clone(), "myriad".as_bytes().to_vec());
-	}: update_name(RawOrigin::Signed(caller), server_id, vec![s as u8])
+		let server_id = Server::<T>::generate_server_id(&account_id, &"myriad".as_bytes().to_vec());
+		let _server = Server::<T>::register(caller_origin.clone(), account_id.clone(), "myriad".as_bytes().to_vec());
+	}: _(RawOrigin::Signed(caller), account_id, server_id, vec![s as u8])
 
 	transfer_owner {
-		let caller: T::AccountId = whitelisted_caller();
+		let caller: T::AccountId = AdminKey::<T>::get();
+		let account_id: T::AccountId = whitelisted_caller();
 		let caller_origin = <T as frame_system::Config>::Origin::from(RawOrigin::Signed(caller.clone()));
-		let server_id = Server::<T>::generate_server_id(&caller, &"myriad".as_bytes().to_vec());
-		let _server = Server::<T>::register(caller_origin.clone(), "myriad".as_bytes().to_vec());
+		let server_id = Server::<T>::generate_server_id(&account_id, &"myriad".as_bytes().to_vec());
+		let _server = Server::<T>::register(caller_origin.clone(), account_id.clone(), "myriad".as_bytes().to_vec());
 		let new_owner: T::AccountId = account("new_owner", 0, SEED);
-	}: transfer_owner(RawOrigin::Signed(caller), server_id, new_owner)
+	}: _(RawOrigin::Signed(caller), account_id, server_id, new_owner)
 
 	unregister {
-		let caller: T::AccountId = whitelisted_caller();
+		let caller: T::AccountId = AdminKey::<T>::get();
+		let account_id: T::AccountId = whitelisted_caller();
 		let caller_origin = <T as frame_system::Config>::Origin::from(RawOrigin::Signed(caller.clone()));
-		let server_id = Server::<T>::generate_server_id(&caller, &"myriad".as_bytes().to_vec());
-		let _server = Server::<T>::register(caller_origin.clone(), "myriad".as_bytes().to_vec());
-	}: unregister(RawOrigin::Signed(caller), server_id)
+		let server_id = Server::<T>::generate_server_id(&account_id, &"myriad".as_bytes().to_vec());
+		let _server = Server::<T>::register(caller_origin.clone(), account_id.clone(), "myriad".as_bytes().to_vec());
+	}: _(RawOrigin::Signed(caller), account_id, server_id)
+
+	transfer_admin_key {
+		let caller: T::AccountId = AdminKey::<T>::get();
+		let account_id: T::AccountId = whitelisted_caller();
+	}: _(RawOrigin::Signed(caller), account_id)
 }
 
 impl_benchmark_test_suite! {Server, crate::mock::ExternalityBuilder::build(), crate::mock::Test}
