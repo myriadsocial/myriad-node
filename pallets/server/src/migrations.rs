6efcb85ee43b9a83a44507074bf544f7eb8cdc6f
abdulhakim2902 <hakim@blocksphere.id>
2022-08-31 08:08:15 +0700

    fix: pallet server migration

--- pallets/server/src/migrations.rs
+++ pallets/server/src/migrations.rs
@@ -1,6 +1,5 @@
-use crate::{AccountIdOf, Config, Pallet, Server, ServerById};
-use frame_support::{pallet_prelude::Decode, traits::Get, weights::Weight};
-use sp_std::vec::Vec;
+use crate::{Config, Pallet, Server, ServerById, ServerByOwner, ServerCount};
+use frame_support::{traits::Get, weights::Weight};
 
 pub fn migrate<T: Config>() -> Weight {
 	use frame_support::traits::StorageVersion;
@@ -8,37 +7,43 @@ pub fn migrate<T: Config>() -> Weight {
 	let mut weight: Weight = 0;
 	let version = StorageVersion::get::<Pallet<T>>();
 
-	if version == 0 {
-		weight = weight.saturating_add(v1::migrate::<T>());
-		StorageVersion::new(1).put::<Pallet<T>>();
+	if version == 1 {
+		weight = weight.saturating_add(v2::migrate::<T>());
+		StorageVersion::new(2).put::<Pallet<T>>();
 	}
 
 	weight
 }
 
-mod v1 {
+mod v2 {
 	use super::*;
 
 	pub fn migrate<T: Config>() -> Weight {
 		let mut weight = T::DbWeight::get().writes(1);
 
-		#[derive(Decode)]
-		pub struct OldServer<AccountId> {
-			pub id: Vec<u8>,
-			pub owner: AccountId,
-			pub name: Vec<u8>,
-		}
+		let server = ServerById::<T>::get(b"myriad".to_vec());
 
-		ServerById::<T>::translate(|_key, old: OldServer<AccountIdOf<T>>| {
-			weight = weight.saturating_add(T::DbWeight::get().reads_writes(1, 1));
-			Some(Server {
-				id: old.id,
-				owner: old.owner,
-				name: old.name,
-				api_url: "https://api.example.com".as_bytes().to_vec(),
-				web_url: "https://web.example.com".as_bytes().to_vec(),
-			})
-		});
+		if let Some(server) = server {
+			weight = weight.saturating_add(T::DbWeight::get().reads_writes(1, 3));
+
+			let server_id = b"0";
+			let new_server = Server::new(
+				server_id,
+				server.get_owner(),
+				server.get_name(),
+				server.get_api_url(),
+				server.get_web_url(),
+			);
+
+			ServerByOwner::<T>::insert(
+				server.get_owner().clone(),
+				server_id.to_vec(),
+				new_server.clone(),
+			);
+			ServerCount::<T>::set(1);
+			ServerById::<T>::insert(server_id.to_vec(), new_server);
+			ServerById::<T>::remove(b"myriad".to_vec());
+		}
 
 		weight
 	}
