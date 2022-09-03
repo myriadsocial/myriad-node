8f05dcc733b333c4dbe3ad0ec281f196da8bd27e
Muhammad Abdul Hakim Shibghatallah <70675129+abdulhakim2902@users.noreply.github.com>
2022-09-03 23:16:39 +0700

    fix: change server_id data type (#143)

--- pallets/server/src/interface.rs
+++ pallets/server/src/interface.rs
@@ -1,7 +1,7 @@
 use sp_std::vec::Vec;
 
 pub trait ServerInfo<T: frame_system::Config> {
-	fn get_id(&self) -> &Vec<u8>;
+	fn get_id(&self) -> u64;
 	fn get_owner(&self) -> &T::AccountId;
 	fn get_api_url(&self) -> &Vec<u8>;
 }
@@ -10,14 +10,14 @@ pub trait ServerProvider<T: frame_system::Config> {
 	type Error;
 	type Server: ServerInfo<T> + sp_std::fmt::Debug;
 
-	fn get_by_id(server_id: &[u8]) -> Option<Self::Server>;
+	fn get_by_id(server_id: u64) -> Option<Self::Server>;
 }
 
 pub trait ServerInterface<T: frame_system::Config> {
 	type Error;
 	type Server;
 
-	fn get_by_id(server_id: &[u8]) -> Option<Self::Server>;
+	fn get_by_id(server_id: u64) -> Option<Self::Server>;
 
 	fn register(owner: &T::AccountId, api_url: &[u8]) -> Result<Self::Server, Self::Error>;
 
--- pallets/server/src/types.rs
+++ pallets/server/src/types.rs
@@ -5,17 +5,17 @@ use sp_std::vec::Vec;
 
 #[derive(Encode, Decode, Clone, Default, RuntimeDebug, PartialEq, Eq, TypeInfo)]
 pub struct Server<AccountId> {
-	pub id: Vec<u8>,
+	pub id: u64,
 	pub owner: AccountId,
 	pub api_url: Vec<u8>,
 }
 impl<AccountId: Clone> Server<AccountId> {
-	pub fn new(id: &[u8], owner: &AccountId, api_url: &[u8]) -> Self {
-		Self { id: id.to_vec(), owner: owner.clone(), api_url: api_url.to_vec() }
+	pub fn new(id: u64, owner: &AccountId, api_url: &[u8]) -> Self {
+		Self { id, owner: owner.clone(), api_url: api_url.to_vec() }
 	}
 
-	pub fn get_id(&self) -> &Vec<u8> {
-		&self.id
+	pub fn get_id(&self) -> u64 {
+		self.id
 	}
 
 	pub fn get_owner(&self) -> &AccountId {
@@ -26,8 +26,8 @@ impl<AccountId: Clone> Server<AccountId> {
 		&self.api_url
 	}
 
-	pub fn set_id(&mut self, server_id: &[u8]) {
-		self.id = server_id.to_vec();
+	pub fn set_id(&mut self, server_id: u64) {
+		self.id = server_id;
 	}
 
 	pub fn set_owner(mut self, account_id: &AccountId) -> Self {
@@ -45,7 +45,7 @@ impl<T, AccountId: Clone> ServerInfo<T> for Server<AccountId>
 where
 	T: frame_system::Config<AccountId = AccountId>,
 {
-	fn get_id(&self) -> &Vec<u8> {
+	fn get_id(&self) -> u64 {
 		self.get_id()
 	}
 
@@ -72,4 +72,4 @@ pub enum OperatorKind {
 
 pub type AccountIdOf<T> = <T as frame_system::Config>::AccountId;
 pub type ServerOf<T> = Server<AccountIdOf<T>>;
-pub type ServerId = Vec<u8>;
+pub type ServerId = u64;
