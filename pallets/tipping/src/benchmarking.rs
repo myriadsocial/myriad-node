fd3455b3e5e2d939237daa3591327a40f96839fe
abdulhakim2902 <hakim@blocksphere.id>
2022-09-10 13:40:50 +0700

    fix: benchmarking tipping pallet

--- pallets/tipping/src/benchmarking.rs
+++ pallets/tipping/src/benchmarking.rs
@@ -1,42 +1,28 @@
-#![cfg_attr(not(feature = "std"), no_std)]
+#![cfg(feature = "runtime-benchmarks")]
 
-mod mock;
+use super::*;
 
-use frame_benchmarking::{account, benchmarks, whitelisted_caller};
+#[allow(unused)]
+use crate::{Config, Pallet as Tipping, TippingInterface};
+use frame_benchmarking::{account, benchmarks, impl_benchmark_test_suite, whitelisted_caller};
 use frame_support::{sp_runtime::SaturatedConversion, traits::Currency};
 use frame_system::RawOrigin;
-use pallet_server::{Config as ServerConfig, Pallet as Server};
-use pallet_tipping::{
-	Call, Config as TippingConfig, Pallet as Tipping, References, TipsBalanceInfo,
-};
 use sp_std::vec;
 
 const SEED: u32 = 0;
-
-pub struct Pallet<T: Config>(Tipping<T>);
-
-pub trait Config: TippingConfig + ServerConfig {}
 
 benchmarks! {
 	send_tip {
 		// Initial account
 		let caller: T::AccountId = whitelisted_caller();
+		let server_id: T::AccountId = account("server_account", 0, SEED);
 
 		// Default balance
 		let balance = 1000000000000000000000u128.saturated_into();
 		let amount = 1000000000000000u128.saturated_into();
 
 		// Caller initial balance
-		let _ = <T as TippingConfig>::Currency::deposit_creating(&caller, balance);
-
-		// Generate server id
-		let server_id = b"0".to_vec();
-
-		// Register server id
-		let server_account: T::AccountId = account("server_account", 0, SEED);
-		let server_origin = <T as frame_system::Config>::Origin::from(RawOrigin::Signed(server_account));
-		let server_api_url = b"https://api.dev.myriad.social".to_vec();
-		let _ = Server::<T>::register(server_origin, server_api_url);
+		let _ = <T as Config>::Currency::deposit_creating(&caller, balance);
 
 		// Send tip
 		let reference_id = b"people_id".to_vec();
@@ -63,22 +49,14 @@ benchmarks! {
 		let tipping_amount = 10000000000000000000u128.saturated_into();
 
 		// Tipping_account_id, account_1, and account_2 initial balance
-		let _ = <T as TippingConfig>::Currency::deposit_creating(&account_1, balance);
-		let _ = <T as TippingConfig>::Currency::deposit_creating(&account_2, balance);
-		let _ = <T as TippingConfig>::Currency::deposit_creating(&tipping_account_id, balance);
-
-		// Generate Server Id
-		let server_id = b"0".to_vec();
-
-		// Register Server
-		let server_origin = <T as frame_system::Config>::Origin::from(RawOrigin::Signed(caller.clone()));
-		let server_api_url = b"https://api.dev.myriad.social".to_vec();
-		let _ = Server::<T>::register(server_origin, server_api_url);
+		let _ = <T as Config>::Currency::deposit_creating(&account_1, balance);
+		let _ = <T as Config>::Currency::deposit_creating(&account_2, balance);
+		let _ = <T as Config>::Currency::deposit_creating(&tipping_account_id, balance);
 
 		// Send Tip by account_1
 		let account_1_origin = <T as frame_system::Config>::Origin::from(RawOrigin::Signed(account_1));
 		let tips_balance_info = TipsBalanceInfo::new(
-			&server_id,
+			&caller,
 			b"people",
 			b"people_id",
 			b"native"
@@ -88,7 +66,7 @@ benchmarks! {
 		// Send Tip by account_2
 		let account_2_origin = <T as frame_system::Config>::Origin::from(RawOrigin::Signed(account_2));
 		let tips_balance_info = TipsBalanceInfo::new(
-			&server_id,
+			&caller,
 			b"user",
 			b"user_id",
 			b"native"
@@ -96,36 +74,30 @@ benchmarks! {
 		let _ = Tipping::<T>::send_tip(account_2_origin, tips_balance_info, tipping_amount);
 
 		// Claim reference data
+		let server_id = caller.clone();
 		let trx_fee = 10000000000000u128.saturated_into();
 		let references = References::new(b"people", &[b"people_id".to_vec()]);
 		let main_references = References::new(b"user", &[b"user_id".to_vec()]);
 		let ft_identifiers = vec![b"native".to_vec()];
-	}: _(RawOrigin::Signed(caller), server_id, references, main_references, ft_identifiers, account_3, trx_fee)
+	}: _(RawOrigin::Signed(caller), server_id, references, main_references, ft_identifiers,
+account_3, trx_fee)
 
 	claim_tip {
 		// Initial account
 		let caller: T::AccountId = whitelisted_caller();
 		let account_1: T::AccountId = account("account", 0, SEED);
 		let account_2: T::AccountId = account("account", 2, SEED);
-		let server_account: T::AccountId = account("server_account", 0, SEED);
+		let server_id: T::AccountId = account("server_account", 0, SEED);
 		let tipping_account_id: T::AccountId = Tipping::<T>::tipping_account_id();
 
 		// Default balance
 		let balance = 1000000000000000000000u128.saturated_into();
 		let tipping_amount = 10000000000000000000u128.saturated_into();
 
-		let _ = <T as TippingConfig>::Currency::deposit_creating(&caller, balance);
-		let _ = <T as TippingConfig>::Currency::deposit_creating(&account_1, balance);
-		let _ = <T as TippingConfig>::Currency::deposit_creating(&account_2, balance);
-		let _ = <T as TippingConfig>::Currency::deposit_creating(&tipping_account_id, balance);
-
-		// Generate Server Id
-		let server_id = b"0".to_vec();
-
-		// Register Server
-		let server_origin = <T as frame_system::Config>::Origin::from(RawOrigin::Signed(server_account));
-		let server_api_url = b"https://api.dev.myriad.social".to_vec();
-		let _ = Server::<T>::register(server_origin.clone(), server_api_url);
+		let _ = <T as Config>::Currency::deposit_creating(&caller, balance);
+		let _ = <T as Config>::Currency::deposit_creating(&account_1, balance);
+		let _ = <T as Config>::Currency::deposit_creating(&account_2, balance);
+		let _ = <T as Config>::Currency::deposit_creating(&tipping_account_id, balance);
 
 		// Send Tip
 		let account_1_origin = <T as frame_system::Config>::Origin::from(RawOrigin::Signed(account_1));
@@ -148,6 +120,7 @@ benchmarks! {
 		let _ = Tipping::<T>::send_tip(account_2_origin, tips_balance_info_user, tipping_amount);
 
 		// Claim Reference
+		let server_origin = <T as frame_system::Config>::Origin::from(RawOrigin::Signed(server_id.clone()));
 		let tx_fee = 10000000000000u128.saturated_into();
 		let _ = Tipping::<T>::claim_reference(
 			server_origin,
@@ -158,5 +131,7 @@ benchmarks! {
 			caller.clone(),
 			tx_fee,
 		);
-	}: _(RawOrigin::Signed(caller), server_id, b"user".to_vec(), b"user_id".to_vec(), vec![b"native".to_vec()])
-}
+	}: _(RawOrigin::Signed(caller), server_id, b"user".to_vec(), b"user_id".to_vec(),
+vec![b"native".to_vec()]) }
+
+impl_benchmark_test_suite! {Server, crate::mock::ExternalityBuilder::build(), crate::mock::Test}
